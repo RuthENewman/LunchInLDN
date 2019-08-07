18922410dab309b754907edd9f1bf8c5f13b7e32
Ruth Newman <ruthnewman@gmail.com>
2019-08-07 14:53:04 +0100

    stylings

--- src/components/RandomChoice.js
+++ src/components/RandomChoice.js
@@ -8,11 +8,12 @@ class RandomChoice extends Component {
       <div className="randomChoice">
           <div className="randomChoice__details">
             <h2 className="randomChoice__details--name">{randomChoice.name}</h2>
+            <h3 className="randomChoice__details--cuisine">{randomChoice.cuisine}</h3>
           </div>
           <img src={randomChoice.image_url}
           alt={randomChoice.name}
           className="randomChoice__image--large"/>
-          <h3 className="randomChoice__details--cuisine">{randomChoice.cuisine}</h3>
+
       </div>
     )
   }
